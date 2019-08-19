bedbc0fba2f9eea2e0c3cd935826d8573b77ced2
DarkWinther <DarkWinther7@gmail.com>
2019-08-19 10:30:13 +0200

    Added header

---
+++ dyreportalen/src/components/header/header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './header.css';
+
+import logo from 'static/images/logo.png';
+import logoText from 'static/images/logo-text.png';
+
+export const Header = () => (
+  <header className="header">
+    <nav role="navigation">
+      <NavLink className="logo" href="/">
+          <img src={logo} alt="" />
+          <img src={logoText} alt="" />
+      </NavLink>
+      <ul>
+        <li><NavLink href="#">Forum</NavLink></li>
+        <li><NavLink href="#">Inspiration</NavLink></li>
+        <li><NavLink href="/annoncer">Annoncer</NavLink></li>
+        <li><NavLink href="/kontakt">Kontakt</NavLink></li>
+        <li>
+            <a className="profile-link" href="#"><i className="material-icons">person</i></a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
