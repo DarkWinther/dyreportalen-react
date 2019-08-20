ceae8403475279717a7f17288aa5275674d9ed3d
DarkWinther <DarkWinther7@gmail.com>
2019-08-20 12:33:15 +0200

    Code first webapi

---
+++ dyreportalen-webapi/dyreportalen-webapi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dyreportalen_webapi.WebApiApplication" Language="C#" %>
--- dyreportalen-www/src/pages/annoncer/annoncer.js
+++ dyreportalen-www/src/pages/annoncer/annoncer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './annoncer.css';
 
 export const Annoncer = () => (
