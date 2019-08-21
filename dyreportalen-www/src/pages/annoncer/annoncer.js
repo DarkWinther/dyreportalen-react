e4111ee9db6c3b6bc3deea3129072af4871bb1ca
DarkWinther <DarkWinther7@gmail.com>
2019-08-21 08:17:54 +0200

    eslint linting

--- dyreportalen-www/src/pages/annoncer/annoncer.js
+++ dyreportalen-www/src/pages/annoncer/annoncer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './annoncer.css';
 
 export const Annoncer = () => (
--- dyreportalen-www/src/pages/kontakt/kontakt.js
+++ dyreportalen-www/src/pages/kontakt/kontakt.js
@@ -6,53 +6,38 @@ import map from 'static/images/maps.png';
 export const Kontakt = () => (
   <main>
     <div className="bigdiv">
-
       <div className="container">
-
         <div className="left-side">
-
           <h3 className="internat-h3">Du skriver til [internat]</h3>
-
           <form id="contact-form">
             <div className="name-email-phone">
               <div className="name-input-container">
-                <label for="customer_name">Navn*</label>
-                <input autocomplete="nope" type="text" id="customer_name" />
+                <label htmlFor="customer_name">Navn*</label>
+                <input autoComplete="nope" type="text" id="customer_name" />
               </div>
-
               <div className="email-input-container">
-                <label for="customer_email">E-mail*</label>
-                <input autocomplete="nope" type="email" id="customer_email" />
+                <label htmlFor="customer_email">E-mail*</label>
+                <input autoComplete="nope" type="email" id="customer_email" />
               </div>
-
               <div className="phone-input-container">
-                <label for="customer_phone">Telefon*</label>
-                <input autocomplete="nope" type="tel" id="customer_phone" />
+                <label htmlFor="customer_phone">Telefon*</label>
+                <input autoComplete="nope" type="tel" id="customer_phone" />
               </div>
             </div>
-
             <div className="message-textarea-container">
-              <label for="customer_message">Besked*</label>
+              <label htmlFor="customer_message">Besked*</label>
               <textarea id="customer_message" cols="80" rows="10"></textarea>
             </div>
-
             <button id="contact-btn" type="submit">Send</button>
-
           </form>
         </div>
-
         <div className="right-side">
-          
-            <h3>Klik på et internat for at skrive til dem</h3>
-                    
-            <div className="maps-container">
-              <img src={map} alt="Maps" className="maps-container-image" />
-            </div>        
-
+          <h3>Klik på et internat for at skrive til dem</h3>
+          <div className="maps-container">
+            <img src={map} alt="Maps" className="maps-container-image" />
+          </div>        
         </div>
-
       </div>
-
     </div>
   </main>
 );
