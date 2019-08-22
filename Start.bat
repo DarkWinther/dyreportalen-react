9e88c545eef2e9a5fc6c71db8fd3c548ddcdd64a
DarkWinther <DarkWinther7@gmail.com>
2019-08-22 11:30:25 +0200

    Start script

---
+++ Start.bat
@@ -0,0 +1,8 @@
+@ECHO off
+
+START "IIS" "C:\Program Files\IIS Express\iisexpress.exe" /path:./dyreportalen-webapi/dyreportalen-webapi /port:63504
+sqlcmd -i ./AddData.sql -S . -E -X
+cd ./dyreportalen-www
+npm run start
+
+PAUSE
